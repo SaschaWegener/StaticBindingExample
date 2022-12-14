083a23f27326cc887a703b41e43dcf305d192b4e
Sascha Wegener <sascha.wegener@macio.de>
2022-12-14 15:09:49 +0100

    Add example for static binding

---
+++ StaticBinding/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace StaticBinding;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ StaticBinding/MainPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticBinding
+{
+    internal class MainPageViewModel
+    {
+        public static string Greeting => "Hello World";
+    }
+}
